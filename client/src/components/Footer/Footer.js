ada785fa4a7b6a2cfbbc0d81810d47cae4616184
PreslavaN99 <139551714+PreslavaN99@users.noreply.github.com>
2024-07-31 13:47:51 +0300

    Working in progress

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './footerStyle.css'
+
+const Footer = () => {
+    return (
+        <footer className={'footer'}>
+            All rights reserved, 2024, Adopt Animal, React App.
+        </footer>
+    );
+}
+
+export default Footer;
--- client/src/components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return(
-        <footer className="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-619a"><div className="u-clearfix u-sheet u-sheet-1">
-        <p className="u-small-text u-text u-text-variant u-text-1">2024 Adopt animal. Sofia, Bulgaria. All Rights Reserved</p>
-      </div></footer>
-    )
-}
